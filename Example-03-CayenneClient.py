8cdce32aae9a1db71cc5258672c7462f9acb6392
jburhenn <jburhenn@mydevices.com>
2016-12-02 17:28:26 -0700

    Added client example that sends and receives data.

---
+++ Example-03-CayenneClient.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import Cayenne
+import time
+
+# Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
+MQTT_USERNAME  = "MQTT_USERNAME"
+MQTT_PASSWORD  = "MQTT_PASSWORD"
+MQTT_CLIENT_ID = "MQTT_CLIENT_ID"
+
+
+# The callback for when a message is received from Cayenne.
+def on_message(message):
+    print("message received: " + str(message))
+    # If there is an error processing the message return an error string, otherwise return nothing.
+    
+client = Cayenne.CayenneMQTTClient()
+client.on_message = on_message
+client.begin(MQTT_USERNAME, MQTT_PASSWORD, MQTT_CLIENT_ID)
+
+i=0
+timestamp = 0
+
+while True:
+    client.loop()
+    
+    if (time.time() > timestamp + 10):
+        client.celsiusWrite(1, i)
+        client.luxWrite(2, i*10)
+        client.hectoPascalWrite(3, i+800)
+        timestamp = time.time()
+        i = i+1
