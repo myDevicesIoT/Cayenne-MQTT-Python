ee80448a19a6cfd4f93719b62e2458f3f7c1d83b
jburhenn <jburhenn@mydevices.com>
2016-12-09 17:31:52 -0700

    Updated Cayenne setup and README.

---
+++ README.rst
@@ -0,0 +1,10 @@
+Cayenne MQTT Python Library
+===========================
+The Cayenne MQTT Python Library provides functions to easily connect to the Cayenne IoT project builder. It supports Python 2.7 or 3.x.
+
+This library installs the `Eclipse Paho MQTT Python client <https://github.com/eclipse/paho.mqtt.python>`_.
+
+
+Additional Cayenne MQTT Libraries
+=================================
+Additional libraries are available for connecting to Cayenne with other languages. These can be found at https://github.com/myDevicesIoT.
--- setup.py
+++ setup.py
@@ -1,15 +1,40 @@
 from setuptools import setup
+from codecs import open
+from os import path
 
-setup(name             = 'Cayenne',
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as readme:
+    long_description = readme.read()
+
+classifiers = ['Development Status :: 3 - Alpha',
+               'Operating System :: MacOS :: MacOS X',
+               'Operating System :: Microsoft :: Windows',
+               'Operating System :: POSIX',
+               'Programming Language :: Python',
+               'License :: OSI Approved :: MIT License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3.5',
+               'Topic :: Software Development',
+               'Topic :: Communications',
+               'Topic :: Internet',
+               'Topic :: Home Automation',
+               'Topic :: System :: Monitoring']
+               
+setup(name             = 'cayenne-mqtt',
       version          = '1.0.0',
       author           = 'myDevices',
       author_email     = 'N/A',
       description      = 'Cayenne MQTT Python Library',
-      long_description = 'The Cayenne MQTT Python Library provides functions to easily connect to the Cayenne IoT project builder.',
+      long_description = long_description,
       license          = 'MIT',
       keywords         = 'myDevices Cayenne MQTT',
       url              = 'https://github.com/myDevicesIoT/Cayenne-MQTT-Python',
-      packages         = ['cayenne'],
+      classifiers      = classifiers,
+      packages         = ['cayenne-mqtt'],
       install_requires = [
         'paho-mqtt',
       ],
