79962d8b0b88f32a6f60934e3d33928846e8bbd0
jburhenn <jburhenn@mydevices.com>
2017-12-12 16:18:16 -0700

    Add new datatypes module.

--- examples/Example-03-CayenneClient.py
+++ examples/Example-03-CayenneClient.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import cayenne.client
+from cayenne import client, datatypes
 import time
 
 # Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
@@ -13,19 +13,20 @@ def on_message(message):
     print("message received: " + str(message))
     # If there is an error processing the message return an error string, otherwise return nothing.
     
-client = cayenne.client.CayenneMQTTClient()
-client.on_message = on_message
-client.begin(MQTT_USERNAME, MQTT_PASSWORD, MQTT_CLIENT_ID)
+cayenne_client = client.CayenneMQTTClient()
+cayenne_client.on_message = on_message
+cayenne_client.begin(MQTT_USERNAME, MQTT_PASSWORD, MQTT_CLIENT_ID)
 
 i=0
 timestamp = 0
 
 while True:
-    client.loop()
+    cayenne_client.loop()
     
     if (time.time() > timestamp + 10):
-        client.celsiusWrite(1, i)
-        client.luxWrite(2, i*10)
-        client.hectoPascalWrite(3, i+800)
+        cayenne_client.celsiusWrite(1, i)
+        cayenne_client.luxWrite(2, i*10)
+        cayenne_client.hectoPascalWrite(3, i+800)
+        cayenne_client.virtualWrite(4, i%2, datatypes.TYPE_DIGITAL_SENSOR, datatypes.UNIT_DIGITAL)
         timestamp = time.time()
         i = i+1
